556403f35c2f2391656189860a6ac1952957f7d9
Ian Guthrie <guthrieim@hendrix.edu>
2022-09-15 15:02:21 -0500

    Humidity added on screen 1

--- sensorprjct/lib/main.dart
+++ sensorprjct/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Todays Temperature'),
     );
   }
 }
@@ -50,6 +50,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  double temp = 0;
+  final environment = EnvironmentSensors();
 
   void _incrementCounter() {
     setState(() {
@@ -96,13 +98,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            StreamBuilder<double>(
+                stream: environment.temperature,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return CircularProgressIndicator();
+                  return Text('The Current Temperature is: ${snapshot.data}');
+                }),
+            StreamBuilder<double>(
+                stream: environment.humidity,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return CircularProgressIndicator();
+                  return Text('The Current Humidity is: ${snapshot.data}');
+                }),
             const SizedBox(
               height: 50,
             ),
@@ -116,15 +123,11 @@ class _MyHomePageState extends State<MyHomePage> {
                             builder: (context) => const SecondRoute()),
                       );
                     },
-                    child: const Text("Open Route"))),
+                    child: const Text("Next Screen"))),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -136,7 +139,7 @@ class SecondRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: const Text('Todays Weather'),
       ),
       body: Center(
         child: ElevatedButton(
