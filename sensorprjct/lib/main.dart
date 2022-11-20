c40ba88d5d1b167d687d555442463f0407c2000f
Simon <reidst@hendrix.edu>
2022-11-19 21:05:27 -0600

    Created a navigation bar
    Created a new home class that contains a globally available navigation bar that can navigate between any screen

--- sensorprjct/lib/main.dart
+++ sensorprjct/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:sensorprjct/temperature.dart';
+import 'package:sensorprjct/weather.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,52 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const NavigationScaffold()
+    );
+  }
+}
+
+class NavigationScaffold extends StatefulWidget {
+  const NavigationScaffold({super.key});
+
+  @override
+  State<NavigationScaffold> createState() => _NavigationScaffoldState();
+}
+
+class _NavigationScaffoldState extends State<NavigationScaffold> {
+
+  int screenIndex = 0;
+  final screens = const [
+    MyHomePage(),
+    WeatherPage()
+  ];
+
+  void updateScreenIndex(int newScreenIndex) {
+    setState(() {
+      screenIndex = newScreenIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[screenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: screenIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.thermostat),
+            label: "Temp"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: "Weather"
+          ),
+        ],
+        onTap: updateScreenIndex,
+        selectedItemColor: Colors.white,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
     );
   }
 }
