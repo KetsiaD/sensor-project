a184cfe5708836be3ec5d00a291ebea7a619d288
TaylorAishman <AishmanTD@hendrix.edu>
2022-12-12 10:05:01 -0600

    Loading bar to fix bug that causes things to not load in all the way

--- sensorprjct/lib/main.dart
+++ sensorprjct/lib/main.dart
@@ -12,13 +12,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const NavigationScaffold()
-    );
+        title: 'Weather App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const NavigationScaffold());
   }
+}
+
+void _LoadingPopUp(BuildContext context) async {
+  showDialog(
+      context: context,
+      builder: (_) {
+        return Dialog(
+          // The background color
+          backgroundColor: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                // The loading indicator
+                LinearProgressIndicator(),
+                SizedBox(
+                  height: 15,
+                ),
+                Text('One Moment, loading')
+              ],
+            ),
+          ),
+        );
+      });
+  await Future.delayed(const Duration(seconds: 5));
+  Navigator.of(context).pop();
 }
 
 class NavigationScaffold extends StatefulWidget {
@@ -29,15 +55,12 @@ class NavigationScaffold extends StatefulWidget {
 }
 
 class _NavigationScaffoldState extends State<NavigationScaffold> {
-
   int screenIndex = 0;
-  final screens = const [
-    MyHomePage(),
-    WeatherPage()
-  ];
+  final screens = const [MyHomePage(), WeatherPage()];
 
   void updateScreenIndex(int newScreenIndex) {
     setState(() {
+      _LoadingPopUp(context);
       screenIndex = newScreenIndex;
     });
   }
@@ -50,15 +73,13 @@ class _NavigationScaffoldState extends State<NavigationScaffold> {
         currentIndex: screenIndex,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.thermostat),
-            label: "Temperature",
-            tooltip: "Temperature and Humidity"
-          ),
+              icon: Icon(Icons.thermostat),
+              label: "Temperature",
+              tooltip: "Temperature and Humidity"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.cloud),
-            label: "Weather",
-            tooltip: "Barometric Pressure"
-          ),
+              icon: Icon(Icons.cloud),
+              label: "Weather",
+              tooltip: "Barometric Pressure"),
         ],
         onTap: updateScreenIndex,
         selectedItemColor: Colors.white,
